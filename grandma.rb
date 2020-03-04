f2a183e3175ac66d629b7d5bcd25228f0520f38f
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-04 21:22:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,8 +12,4 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-speak_to_grandma = "I LOVE YOU GRANDMA!"
 
-if speak_to_grandma == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-end
