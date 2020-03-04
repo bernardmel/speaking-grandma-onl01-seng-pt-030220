63b4a510b1efa13dcdf9c17d33f745e88116842b
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-04 21:18:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,19 +14,17 @@
 
 if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-end
 
-if speak_to_grandma == "Hi Nana, how are you?"
+elsif speak_to_grandma == "Hi Nana, how are you?"
   puts "HUH?! SPEAK UP, SONNY!"
 
-elsif speak_to_grandma == "Hi!"
+else speak_to_grandma == "Hi!"
   puts "HUH?! SPEAK UP, SONNY!"
 
-end
 
 else speak_to_grandma == "WHAT DID YOU EAT TODAY?"
   puts "NO, NOT SINCE 1938!"
 
-elsif speak_to_grandma == "WHAT?"
+else speak_to_grandma == "WHAT?"
   puts "NO, NOT SINCE 1938"
 end
