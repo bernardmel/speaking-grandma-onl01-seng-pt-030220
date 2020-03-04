827b59bc7ed6dd7305f2745289468f55d58f4fd0
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-04 21:38:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMKIN!"
+  elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH! SPEAK UP, SONNY!"
   end
 end
